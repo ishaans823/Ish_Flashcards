f04c468a0b787a674bbc66867cc62591190b11d0
Ishaan Shah <ishaans@umich.edu>
2021-03-21 22:54:17 -0400

    database done

--- app/src/main/java/com/example/ishaansflashcards/MainActivity.java
+++ app/src/main/java/com/example/ishaansflashcards/MainActivity.java
@@ -11,16 +11,30 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    FlashcardDatabase flashcardDatabase;
+    List<Flashcard> flashcardList;
+    int index_curr = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        flashcardDatabase = new FlashcardDatabase(getApplicationContext());
         String s1 = getIntent().getStringExtra("string1");
         String s2 = getIntent().getStringExtra("string2");
+        flashcardList = flashcardDatabase.getAllCards();
 
+        //checking if list is empty
+        if(flashcardList.size() > 0 && flashcardList != null) {
+            ((TextView) findViewById(R.id.flashcard_question)).setText(flashcardList.get(0).getQuestion());
+            ((TextView) findViewById(R.id.flashcard_answer)).setText(flashcardList.get(0).getAnswer());
+        }
+        //checking if list is empty
 
         findViewById(R.id.addingQ).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +60,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        findViewById(R.id.next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // make sure we have cards + increment
+                if(flashcardList.size()  == 0) {
+                    return;
+                }
+                index_curr++;
+                // no index error
+                if(index_curr >= flashcardList.size()) {
+                    index_curr = 0;
+                }
+                // set data
+                flashcardList = flashcardDatabase.getAllCards();
+                Flashcard flashcard = flashcardList.get(index_curr);
+                ((TextView) findViewById(R.id.flashcard_question)).setText(flashcard.getQuestion());
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(flashcard.getAnswer());
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+
+
+            }
+        });
 
     }
 
@@ -59,6 +96,10 @@ public class MainActivity extends AppCompatActivity {
                 ((TextView) findViewById(R.id.flashcard_answer)).setText(string2);
                 findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
                 findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                flashcardDatabase.insertCard(new Flashcard(string1, string2));
+                flashcardList = flashcardDatabase.getAllCards();
+
+
             }
 
 
